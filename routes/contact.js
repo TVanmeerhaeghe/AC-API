c648c9f31e05667d7fc4cecefa54b9bc6e32848a
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-05-30 11:54:54 +0200

    fix: :bug: FIxed contact endpoints

--- routes/contact.js
+++ routes/contact.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const contactCtrl = require('../controllers/contactController');
 
-router.post('/contacts', contactCtrl.createContact);
-router.get('/contacts', contactCtrl.getAllContacts);
-router.get('/contacts/:id', contactCtrl.getContactById);
-router.delete('/contacts/:id', contactCtrl.deleteContact);
+router.post('/', contactCtrl.createContact);
+router.get('/', contactCtrl.getAllContacts);
+router.get('/:id', contactCtrl.getContactById);
+router.delete('/:id', contactCtrl.deleteContact);
 
 module.exports = router;
