4a82f534937a70e779430b206dc176d01a258ca2
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-06-16 13:49:25 +0200

    fix: :bug: Fixed security on invocie endpoint and order on product endpoint

--- routes/product.js
+++ routes/product.js
@@ -13,8 +13,8 @@ router.get('/search', productController.searchProducts);
 
 router.post('/', verifyToken, isAdmin, cpUpload, productController.createProduct);
 router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getProductById);
 router.get('/sold-revenue', verifyToken, isAdmin, productController.getSoldRevenue);
+router.get('/:id', productController.getProductById);
 router.put('/:id', verifyToken, isAdmin, cpUpload, productController.updateProduct);
 router.delete('/:id', verifyToken, isAdmin, productController.deleteProduct);
 
